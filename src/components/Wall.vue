11abc1031dbdff85409bd2fe4be00a3b323775dd
Karma Blackshaw <irenesejah29@gmail.com>
2021-03-19 12:44:08 +0900

    update: add wall

--- src/components/Wall.vue
+++ src/components/Wall.vue
@@ -1,15 +1,29 @@
 <template>
   <div>
-    Wall
+    <template v-for="i in 5">
+      <ActivtyCard :key="i" />
+
+      <base-hr
+        v-if="i < 5"
+        :key="i"
+        class="mx-4"
+      />
+    </template>
   </div>
 </template>
 
 <script>
-export default {
+import ActivtyCard from '@/components/ActivityCard'
 
+export default {
+  components: {
+    ActivtyCard
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.activity__horizontal-rule {
+  margin: 20px;
+}
 </style>
