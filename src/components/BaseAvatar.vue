d6b22562d4f69271479a416f43a2fcc9472cd0dc
Karma Blackshaw <irenesejah29@gmail.com>
2021-03-19 12:41:29 +0900

    update: add avatar

---
+++ src/components/BaseAvatar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="base-avatar">
+    <img
+      :src="src"
+      v-bind="{ ...avatarSize }"
+      class="base-avatar__img"
+      alt=""
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: [Number, String],
+      default: 30
+    },
+    src: {
+      type: [String],
+      default: ''
+    }
+  },
+
+  computed: {
+    avatarSize () {
+      return {
+        height: this.size + 'px',
+        width: this.size + 'px'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.base-avatar {
+  .base-avatar__img {
+    border-radius: 50%;
+  }
+}
+</style>
